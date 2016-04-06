b81b0f86cd338fae53e7966ad7c5c77a34f835fd
Allenz <allenz8512@gmail.com>
2016-04-06 21:41:43 +0800

    修复注解Observable的注解

--- annotation/src/main/java/me/allenzjl/domaincache/Observable.java
+++ annotation/src/main/java/me/allenzjl/domaincache/Observable.java
@@ -1,8 +1,17 @@
 package me.allenzjl.domaincache;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by Allen on 2016/4/6.
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+@Documented
 public @interface Observable {
 
     int value() default CacheStrategy.READ_CACHE_ONLY;
